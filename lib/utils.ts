25e8d1a80e30b9ae55a5870515b6962ab5ca4e19
Said Yeter <kordiseps@gmail.com>
2024-03-18 01:41:56 +0300

    Add icons and item component #1

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,55 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function generateColor(val: string) {
+  return colors[calculate(val) % colors.length]
+}
+
+// taken from https://stackoverflow.com/a/7616484
+function calculate(val: string) {
+
+  let hash = 0, i, chr;
+  if (val?.length === 0) return hash;
+  for (i = 0; i < val?.length; i++) {
+    chr = val.charCodeAt(i);
+    hash = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  if (hash < 0) {
+    hash = -hash
+  }
+  return hash;
+}
+
+const colors = [
+  "#A6D0DD",
+  "#FF6969",
+  "#FFD3B0",
+  "#E8A0BF",
+  "#BA90C6",
+  "#C7E9B0",
+  "#CCD5AE",
+  "#FFB4B4",
+  "#FFACAC",
+  "#FFAACF",
+  "#FFCEFE",
+  "#F8CBA6",
+  "#CDE990",
+  "#8DCBE6",
+  "#FD8A8A",
+  "#BCEAD5",
+  "#9ED5C5",
+  "#BCCEF8",
+  "#ABD9FF",
+  "#FFABE1",
+  "#B1D7B4",
+  "#F7ECDE",
+  "#B2C8DF",
+  "#C4D7E0",
+  "#C7D36F",
+  "#E0DECA",
+  "#CDC2AE",
+  "#92B4EC",
+]
